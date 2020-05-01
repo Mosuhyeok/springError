2158efc5d32d33b6de47dfb9adb7e387dab4ac1e
Mosuhyeok <msv31v@naver.com>
2020-05-01 17:16:14 +0900

    hdsg

---
+++ src/main/java/d/demodlkfksdkf/DemodlkfksdkfApplication.java
@@ -0,0 +1,13 @@
+package d.demodlkfksdkf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodlkfksdkfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemodlkfksdkfApplication.class, args);
+    }
+
+}
